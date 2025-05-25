1a02cb08db2e8cbd5f492488c47d23a8181d80be
Jonas Nyrup <jnyrup@gmail.com>
2025-05-25 16:25:45 +0200

    WIP: extract fallbackArm to local function

--- src/Riok.Mapperly/Descriptors/Mappings/DerivedTypeSwitchMapping.cs
+++ src/Riok.Mapperly/Descriptors/Mappings/DerivedTypeSwitchMapping.cs
@@ -1,4 +1,5 @@
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Riok.Mapperly.Emit.Syntax;
 using Riok.Mapperly.Helpers;
@@ -18,23 +19,44 @@ public class DerivedTypeSwitchMapping(ITypeSymbol sourceType, ITypeSymbol target
 
     public override IEnumerable<StatementSyntax> BuildBody(TypeMappingBuildContext ctx)
     {
-        // _ => throw new ArgumentException(msg, nameof(ctx.Source)),
+        // static ADto Throw(A source) => throw new ArgumentException(msg, nameof(ctx.Source));
         var sourceTypeExpr = ctx.SyntaxFactory.Invocation(MemberAccess(ctx.Source, GetTypeMethodName));
-        var fallbackArm = SwitchArm(
-            DiscardPattern(),
-            ThrowArgumentExpression(
-                InterpolatedString(
-                    $"Cannot map {sourceTypeExpr} to {TargetType.ToDisplayString()} as there is no known derived type mapping"
-                ),
-                ctx.Source
+        var throwHelper = LocalFunctionStatement(FullyQualifiedIdentifier(TargetType).AddTrailingSpace(), Identifier("Throw"))
+            .WithLeadingTrivia()
+            .WithModifiers(TokenList(TrailingSpacedToken(SyntaxKind.StaticKeyword)))
+            .WithParameterList(
+                ParameterList(
+                    Token(SyntaxKind.OpenParenToken),
+                    SingletonSeparatedList(Parameter(SourceType.FullyQualifiedIdentifierName(), ctx.Source.ToString())),
+                    Token(SyntaxKind.CloseParenToken)
+                )
             )
-        );
+            .WithExpressionBody(
+                ArrowExpressionClause(
+                        ThrowArgumentExpression(
+                                InterpolatedString(
+                                    $"Cannot map {sourceTypeExpr} to {TargetType.ToDisplayString()} as there is no known derived type mapping"
+                                ),
+                                ctx.Source
+                            )
+                            .AddLeadingSpace()
+                    )
+                    .AddLeadingSpace()
+            )
+            .WithSemicolonToken(Token(SyntaxKind.SemicolonToken));
+
+        // _ => Throw(source)
+        var fallbackArm = SwitchArm(DiscardPattern(), ctx.SyntaxFactory.Invocation("Throw", ctx.Source));
 
         // source switch { A x => MapToADto(x), B x => MapToBDto(x) }
         var (typeArmContext, typeArmVariableName) = ctx.WithNewSource();
         var arms = typeMappings.Select(x => BuildSwitchArm(typeArmVariableName, x.SourceType, x.Build(typeArmContext))).Append(fallbackArm);
         var switchExpression = ctx.SyntaxFactory.Switch(ctx.Source, arms);
-        return [ctx.SyntaxFactory.Return(switchExpression)];
+        return
+        [
+            ctx.SyntaxFactory.Return(switchExpression).AddTrailingLineFeed(0),
+            throwHelper.AddLeadingLineFeed(ctx.SyntaxFactory.Indentation),
+        ];
     }
 
     private SwitchExpressionArmSyntax BuildSwitchArm(string typeArmVariableName, ITypeSymbol type, ExpressionSyntax mapping)
